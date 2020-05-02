19c421fefe83564a52801c27de9c1d6ad2a0fde7
Fikayo <fik4christ@yahoo.com>
2020-05-02 23:06:16 +0100

    Configuration Added

--- src/pages/Tab1.test.tsx
+++ src/pages/Tab1.test.tsx
@@ -2,11 +2,6 @@ import React from "react";
 import { render } from "@testing-library/react";
 import Tab1 from "./Tab1";
 
-interface Task {
-  id: number;
-  name: string;
-}
-
 test("Page title is Task Manager", async () => {
   const { findByText } = render(<Tab1 />);
   await findByText("Task Manager");
